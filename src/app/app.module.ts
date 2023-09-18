8ce62e1970db1bf323794386e83f48ff3a879e59
Ronald Allan <115074375+ronaldpaguntalan@users.noreply.github.com>
2023-09-18 22:09:57 +0800

    Angular Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { initializeApp } from 'firebase/app';
+import { environment } from './environments/environment.prod';
+
+const firebaseConfig = environment.firebaseConfig;
+const app = initializeApp(firebaseConfig);
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
